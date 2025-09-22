e0400267ee8d7619a68cc270558f4cf6a27f1771
Rodrigo Zúñiga <rodrigozuniga3010@gmail.com>
2025-09-21 19:51:55 -0500

    refactor: update dbCredentials configuration in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,15 +5,15 @@ export default defineConfig({
   schema: "./src/db/schema/index.ts", // ruta donde defines tus tablas
   out: "./drizzle", // carpeta donde se generan migraciones
   dbCredentials: {
-    //  // local postgres (mac)
-    // host: 'localhost',
-    // port: 5432,
-    // user: 'postgres',
-    // password: 'postgres',
-    // database: 'lexia-saas',
-    // ssl: false,         // local, sin certificados
+    // local postgres (mac)
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'lexia-saas',
+    ssl: false,
 
-    url: "postgres://postgres:JglaqZz7LOf78OlC5ryS@lexia-aurora-psql-develop.cluster-c6dmuuo8gatt.us-east-1.rds.amazonaws.com:5432/files_db?sslmode=no-verify",
+    // url: "postgres://postgres:JglaqZz7LOf78OlC5ryS@lexia-aurora-psql-develop.cluster-c6dmuuo8gatt.us-east-1.rds.amazonaws.com:5432/files_db?sslmode=no-verify",
 
   },
   migrations: {
