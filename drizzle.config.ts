6a578c3e704c706fb75e6619679c8ea3ecc9dbdb
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2025-09-24 11:40:57 -0500

    agregando Readmes y modificado el config de drizzle y wrangler

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,25 @@
+// CREATE DATABASE "lexia-saas";
+// CREATE USER lexia_user WITH PASSWORD 'lexia_password';
+// GRANT ALL PRIVILEGES ON DATABASE "lexia-saas" TO lexia_user;
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/db/schema/index.ts", // ruta donde defines tus tablas
-  out: "./drizzle", // carpeta donde se generan migraciones
-  dbCredentials: {
-    // local postgres (mac)
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'lexia-saas',
-    ssl: false,
+	dialect: "postgresql",
+	schema: "./src/db/schema/index.ts", // ruta donde defines tus tablas
+	out: "./drizzle", // carpeta donde se generan migraciones
+	dbCredentials: {
+		// local postgres (mac)
+		host: "localhost",
+		port: 5432,
+		user: "lexia_user",
+		password: "lexia_password",
+		database: "lexia-saas",
+		ssl: false,
 
-    // url: "postgres://postgres:JglaqZz7LOf78OlC5ryS@lexia-aurora-psql-develop.cluster-c6dmuuo8gatt.us-east-1.rds.amazonaws.com:5432/files_db?sslmode=no-verify",
-
-  },
-  migrations: {
-    table: "__drizzle_migrations_files", // nombre de la tabla de control
-    schema: "drizzle", // schema donde se crea esa tabla
-  },
+		// url: "postgres://postgres:JglaqZz7LOf78OlC5ryS@lexia-aurora-psql-develop.cluster-c6dmuuo8gatt.us-east-1.rds.amazonaws.com:5432/files_db?sslmode=no-verify",
+	},
+	migrations: {
+		table: "__drizzle_migrations_files", // nombre de la tabla de control
+		schema: "drizzle", // schema donde se crea esa tabla
+	},
 });
